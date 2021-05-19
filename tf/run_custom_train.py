1d1e9dd7fed13367f736d989c51763ab5569fb31
cursedseraphim <christian.steinparz@gmail.com>
2021-05-19 11:35:10 +0200

    updates and fixes on icg exp pc

--- tf/run_custom_train.py
+++ tf/run_custom_train.py
@@ -1,16 +1,19 @@
 import custom_train
 import argparse
+from multiprocessing import freeze_support
 
-argparser = argparse.ArgumentParser(description=\
-'Tensorflow pipeline for training Leela Chess.')
-argparser.add_argument('--cfg',
-                        type=argparse.FileType('r'),
-                        help='yaml configuration with training parameters')
-argparser.add_argument('--output',
-                        type=str,
-                        help='file to store weights in')
-cmd = argparser.parse_args()
-cmd.cfg = open('128x10-t60-2.yaml')
-cmd.net = '128x10-t60-2-5300.pb.gz'
+if __name__ == '__main__':
+    freeze_support()
+    argparser = argparse.ArgumentParser(description=\
+    'Tensorflow pipeline for training Leela Chess.')
+    argparser.add_argument('--cfg',
+                            type=argparse.FileType('r'),
+                            help='yaml configuration with training parameters')
+    argparser.add_argument('--output',
+                            type=str,
+                            help='file to store weights in')
+    cmd = argparser.parse_args()
+    cmd.cfg = open('128x10-t60-2.yaml')
+    cmd.net = '128x10-t60-2-5300.pb.gz'
 
-custom_train.main(cmd)
+    custom_train.main(cmd)
